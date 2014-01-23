a51360f5d691c8c08f30f7f0543e4a4075807acb
jc <emclab2011@gmail.com>
2014-01-23 15:56:41 -0600

    JC: added style. More CN.

--- db/migrate/20130804210247_create_supplierx_suppliers.rb
+++ db/migrate/20130804210247_create_supplierx_suppliers.rb
@@ -3,23 +3,23 @@ class CreateSupplierxSuppliers < ActiveRecord::Migration
     create_table :supplierx_suppliers do |t|
       t.string :name
       t.string :short_name
-      t.text :contact_info
       t.string :contact_name
       t.string :phone
-      t.string :fax
       t.string :cell
-      t.string :email
       t.text :address
       t.string :web
-      t.text :main_product
-      t.string :short_comment
-      t.date :supply_since
-      t.boolean :active, :default => true
       t.integer :last_updated_by_id
+      t.text :main_product
+      t.date :supply_since
+      t.string :short_comment     
+      t.timestamps
+      t.boolean :active, :default => true
+      t.string :fax
+      t.string :email
       t.integer :quality_system_id
       t.text :note
-
-      t.timestamps
+      t.text :contact_info
+   
     end
     
     add_index :supplierx_suppliers, :name
