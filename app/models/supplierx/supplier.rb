1c27c91526a9c7e2251f4c1f39d87643b30844b1
jc <emclab2011@gmail.com>
2015-02-23 15:37:12 -0600

    JC: rename quality_system to quality_system_name. removed presence of contact_name. spec passes.

--- app/models/supplierx/supplier.rb
+++ app/models/supplierx/supplier.rb
@@ -1,10 +1,10 @@
 module Supplierx
   class Supplier < ActiveRecord::Base
-    attr_accessor :active_noupdate
-    attr_accessible :active, :address, :cell, :contact_name, :contact_info, :email, :fax, :last_eval_date, :last_updated_by_id, :main_product, :name, :phone, :quality_system, 
+    attr_accessor :active_noupdate, :quality_system_name
+    attr_accessible :active, :address, :cell, :contact_name, :contact_info, :email, :fax, :last_eval_date, :last_updated_by_id, :main_product, :name, :phone, :quality_system_name, 
                      :short_name, :supply_since, :web, :note, :short_comment, :quality_system_id, 
                      :as => :role_new
-    attr_accessible :active, :address, :cell, :contact_name, :contact_info, :email, :fax, :last_eval_date, :last_updated_by_id, :main_product, :name, :phone, :quality_system, 
+    attr_accessible :active, :address, :cell, :contact_name, :contact_info, :email, :fax, :last_eval_date, :last_updated_by_id, :main_product, :name, :phone, :quality_system_name, 
                      :short_name, :supply_since, :web, :note, :short_comment, :quality_system_id, 
                      :active_noupdate,
                      :as => :role_update
@@ -14,8 +14,8 @@ module Supplierx
     
     email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
       
-    validates_presence_of :name, :short_name, :contact_name, :address, :phone
-    validates :name, :short_name, :presence => true, :uniqueness => {:case_sensitive => false, :message => I18n.t('Must be unique!')}
+    validates_presence_of :name, :short_name, :phone
+    validates :name, :short_name, :uniqueness => {:case_sensitive => false, :message => I18n.t('Must be unique!')}
     validates :email, :format     => { :with => email_regex, :message => '电邮格式错误！' },
                       :uniqueness => { :case_sensitive => false, :message => '电邮已占用！' },
                       :if => 'email.present?'
