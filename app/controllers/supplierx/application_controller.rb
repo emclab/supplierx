b0de3a22794a25f6618efc48cf61f172276d7266
JC <jc@email.com>
2016-06-30 11:15:49 -0500

    JC: added 2 helper methods.

--- app/controllers/supplierx/application_controller.rb
+++ app/controllers/supplierx/application_controller.rb
@@ -14,6 +14,7 @@ module Supplierx
     after_action :delete_session_variable, :only => [:create, :update]   #for parent_record_id & parent_resource in check_access_right
     before_action :page_params, :only => :index
     
+    helper_method :return_users, :return_misc_definitions
     
     protected
   
